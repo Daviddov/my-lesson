8ff563bcfd2d5388799969480f6525ecf6eadddd
DavidDov <109440152+Daviddov@users.noreply.github.com>
2022-08-11 14:57:37 +0300

    1
    new commit

--- progect-JSON_AJAX/requsetFunction.js
+++ progect-JSON_AJAX/requsetFunction.js
@@ -1,17 +1,32 @@
-function requestSender(type, url, funHandeler, header = {}, body=null ){
+function requestSender(type, url, funHandeler, header = {}, body = null) {
     const req = new XMLHttpRequest();
-    req.open(type,url)
+    req.open(type, url)
 
     Object.keys(header).forEach((key) => {
         req.setRequestHeader(key, header[key]);
     });
-    
+
     req.setRequestHeader("Content-type", "application/json");
     req.send(body)
-    req.onload=()=>{
-        if(req.responseText){
-      const response = JSON.parse(req.responseText);
-     funHandeler(response);
+    req.onload = () => {
+        if (req.responseText) {
+            const response = JSON.parse(req.responseText);
+            funHandeler(response);
+        }
     }
+}
+
+
+class Course {
+    constructor(element) {
+        const div = document.createElement('div');
+        div.innerText = element.title;
+        const course = document.body.appendChild(div);
+        course.onclick = () => {
+            let idCourse =  element._id;
+            localStorage.setItem('idCourse', idCourse);
+            window.location.href = './course-details.html';
+        }
     }
+
 }
