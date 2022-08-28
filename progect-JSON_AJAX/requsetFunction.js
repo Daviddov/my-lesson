160a5f59c2993e73542f1c9de1f8a53343559948
DavidDov <109440152+Daviddov@users.noreply.github.com>
2022-08-28 16:17:20 +0300

    sun

--- progect-JSON_AJAX/requsetFunction.js
+++ progect-JSON_AJAX/requsetFunction.js
@@ -1,4 +1,8 @@
-function requestSender(type, url, funHandeler, header = {}, body = null) {
+let theRequest = {}
+let requestStatusText;
+let requestRespunseText;
+
+function requestSender(type, url, resHandeler, header = {}, body = null) {
     const req = new XMLHttpRequest();
     req.open(type, url)
 
@@ -9,24 +13,108 @@ function requestSender(type, url, funHandeler, header = {}, body = null) {
     req.setRequestHeader("Content-type", "application/json");
     req.send(body)
     req.onload = () => {
-        if (req.responseText) {
-            const response = JSON.parse(req.responseText);
-            funHandeler(response);
+        theRequest = req
+        console.log(theRequest)
+
+        switch (theRequest.status) {
+            case 201:
+                alert('Created')
+                resHandeler()
+                break;
+            case 401 || 500 :
+                alert(JSON.stringify(theRequest.responseText));
+                break;
+            case 400 :
+                alert(JSON.stringify(theRequest.responseText));
+                break;
+            case 200:
+                theRequest.data = JSON.parse(req.responseText)
+            resHandeler(theRequest.data);
+                break;
+        
+            default:
+                break;
         }
+
     }
 }
 
-
 class Course {
     constructor(element) {
-        const div = document.createElement('div');
-        div.innerText = element.title;
-        const course = document.body.appendChild(div);
+        const section = document.createElement('section');
+        section.innerText = element.title;
+        const course = conteiner.appendChild(section);
+
         course.onclick = () => {
-            let idCourse =  element._id;
-            localStorage.setItem('idCourse', idCourse);
+            const idCourse = element._id;
+            sessionStorage.setItem('idCourse', idCourse);
             window.location.href = './course-details.html';
         }
     }
+
+}
+
+function randerToHtml(name, tag) {
+    const element = name;
+    const section = document.createElement(tag);
+    section.innerText = element;
+    const course = conteiner.appendChild(section);
+}
+
+
+function courseDetails(res) {
+    console.log(res.data[0]);
+    if (res) {
+        courseData = res.data[0]
+        randerToHtml('Title: ' + courseData.title, 'h1')
+        randerToHtml(
+            `description: ${courseData.description}
+        materialsNeeded: ${courseData.materialsNeeded}
+        overallRating: ${courseData.overallRating}
+        creator: ${courseData.user.fullName}
+        `, 'p');
+
+
+
+        for (const review of courseData.reviews) {
+            randerToHtml(`Review
+    date: ${review.postedOn}
+    rating: ${review.rating}
+    review: ${review.review}
+    creator: ${review.user.fullName}`, 'p');
+        }
+
+
+        for (const step of courseData.steps) {
+            randerToHtml(`Rating
+        title: ${step.title}
+        description: ${step.description}
+        stepNumber: ${step.stepNumber}`, 'p')
+        };
+    }
+}
+
+function checkLogin() {
+    let user = JSON.parse(localStorage.getItem('user'));
+    if (user != null) {
+        const loginDate = new Date(user.date);
+        const nowDate = new Date;
+        const timePass = nowDate - loginDate;
+        if (timePass > 60 * 60 * 1000) {
+            logout()
+        } else {
+            const header = document.querySelector('#header')
+            const p = document.createElement('h4');
+            p.innerText = 'Welcome ' + user.fullName;
+            const name = header.appendChild(p)
+        }
+    } else {
+
+            location.href = './index.html'
+        }
+    }
+
+function logout() {
+    localStorage.setItem('user', null);
 
 }
